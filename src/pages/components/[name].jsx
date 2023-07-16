7d9801e45cf9abe56890c42e11dd49fb7d936ee2
lerte smith <lerte@qq.com>
2023-07-17 02:56:44 +0800

    fix build error

--- src/pages/components/[name].jsx
+++ src/pages/components/[name].jsx
@@ -1,9 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 export default () => {
   const { name } = useParams()
