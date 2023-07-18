43be2011c4ad0f28b77a445ec1f84b3e0e386579
lerte smith <lerte@qq.com>
2023-07-18 18:24:50 +0800

    fix build dynamic import file

--- src/pages/[actify]/[name].jsx
+++ src/pages/[actify]/[name].jsx
@@ -4,6 +4,8 @@ import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import rehypeRaw from 'rehype-raw'
 import Code from '@/packages/components/Code'
+import { Pencil } from 'lucide-react'
+import Button from '@/packages/components/Button'
 
 export default () => {
   const params = useParams()
@@ -12,8 +14,7 @@ export default () => {
 
   useEffect(() => {
     async function loadData() {
-      const docPath = `../../docs/${params.actify}/${params.name}.md`
-      const { attributes, markdown } = await import(docPath)
+      const { attributes, markdown } = await import(`../../docs/${params.actify}/${params.name}.md`)
       setAttributes(attributes)
       setMarkdown(markdown)
     }
@@ -21,23 +22,36 @@ export default () => {
   }, [params])
 
   return (
-    <ReactMarkdown
-      className="prose max-w-full dark:prose-invert prose-pre:p-0 [&_pre]:!mb-0"
-      children={markdown}
-      rehypePlugins={[rehypeRaw]}
-      remarkPlugins={[[remarkGfm, { singleTilde: false }]]}
-      components={{
-        code({ node, inline, className, children, ...props }) {
-          const match = /language-(\w+)/.exec(className || '')
-          return !inline && match ? (
-            <Code code={String(children).replace(/\n$/, '')} language={match[1]} />
-          ) : (
-            <code className={className} {...props}>
-              {children}
-            </code>
-          )
-        }
-      }}
-    />
+    <>
+      <ReactMarkdown
+        className="prose mb-4 max-w-full dark:prose-invert prose-pre:p-0 [&_pre]:!mb-0"
+        children={markdown}
+        rehypePlugins={[rehypeRaw]}
+        remarkPlugins={[[remarkGfm, { singleTilde: false }]]}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <Code code={String(children).replace(/\n$/, '')} language={match[1]} />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          }
+        }}
+      />
+      <Button
+        variant="text"
+        target="_blank"
+        trailing-icon
+        href={`https://github.com/actifyjs/actify/blob/main/src/docs/${params.actify}/${params.name}.md`}
+      >
+        Edit this page on GitHub
+        <div slot="icon" className="flex items-center">
+          <Pencil />
+        </div>
+      </Button>
+    </>
   )
 }
