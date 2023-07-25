925b0769973457c4ef6ffe4fd3886fa3ca0da525
lerte smith <lerte@qq.com>
2023-07-25 22:28:22 +0800

    update prettierrc

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  tabWidth: 2,
-  semi: false,
-  printWidth: 120,
-  singleQuote: true,
-  trailingComma: 'none',
-  bracketSameLine: false,
-  plugins: [require('prettier-plugin-tailwindcss')],
-}
