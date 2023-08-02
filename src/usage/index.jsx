b7d6bce50e710434848f73f297e41fba4b03056a
lerte smith <lerte@qq.com>
2023-08-02 10:27:41 +0800

    update

--- src/usage/index.jsx
+++ src/usage/index.jsx
@@ -1,7 +1,7 @@
 import { lazy, Suspense } from 'react'
 
 const Usage = ({ name }) => {
-  const LazyComponent = lazy(() => import(`./${name}.jsx`))
+  const LazyComponent = lazy(() => import(`../usage/${name}.jsx`))
   return (
     <Suspense>
       <LazyComponent name={name} />
