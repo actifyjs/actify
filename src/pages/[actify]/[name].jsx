0760f49b4aeefe095169e06fa3fa9a733cf58576
lerte smith <lerte@qq.com>
2023-08-02 10:37:08 +0800

    uppercase sensitive

--- src/pages/[actify]/[name].jsx
+++ src/pages/[actify]/[name].jsx
@@ -6,7 +6,7 @@ import rehypeRaw from 'rehype-raw'
 import Code from '@/packages/components/Code'
 import { Pencil } from 'lucide-react'
 import Button from '@/packages/components/Button'
-import Usage from '@/src/Usage'
+import Usage from '@/src/usage'
 
 export default () => {
   const params = useParams()
