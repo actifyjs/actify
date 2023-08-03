50f0c13172f5970a2fe4c17610ba0d461617488f
lerte smith <lerte@qq.com>
2023-08-04 01:53:31 +0800

    lucide icon

--- src/usage/icon.jsx
+++ src/usage/icon.jsx
@@ -1,6 +1,45 @@
-import { Icon } from '@/packages/components'
+import icons from '@/src/icons.json'
+import { useEffect, useState, useTransition } from 'react'
+import { Icon, TextField } from '@/packages/components'
 
 export default (props) => {
+  const [isPending, startTransition] = useTransition()
+  const [filterIcons, setFilterIcons] = useState([])
   const { icon, name, ...rest } = props
-  return <Icon name={icon} {...rest} />
+
+  useEffect(() => {
+    setFilterIcons(icons)
+  }, [])
+
+  const handleInput = (e) => {
+    const { value } = e.target
+    const reg = new RegExp(value, 'i')
+    startTransition(() => {
+      setFilterIcons(icons.filter((item) => reg.test(item)))
+    })
+  }
+
+  // 复制到剪切板
+  const cliptoboard = (str) => {
+    navigator.clipboard.writeText(str)
+  }
+
+  return (
+    <>
+      <TextField className="w-full" onInput={handleInput} placeholder={`Search icon in ${icons.length}`} />
+      <div className="mt-2 gap-2 grid grid-cols-[repeat(auto-fill,minmax(56px,1fr))]">
+        {filterIcons.map((item) => (
+          <Icon
+            size={36}
+            key={item}
+            name={item}
+            color="primary"
+            onClick={cliptoboard(item)}
+            className="flex items-center justify-center cursor-pointer p-2 bg-black/10 dark:bg-white/10 rounded-md"
+          />
+        ))}
+      </div>
+      <Icon name="Home" {...rest} />
+    </>
+  )
 }
