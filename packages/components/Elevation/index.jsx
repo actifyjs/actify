2cab8db8526cbef1e349ab40fdfd776f4daff7ea
lerte smith <lerte@qq.com>
2023-08-04 23:29:27 +0800

    rewrite m3 elevation

--- packages/components/Elevation/index.jsx
+++ packages/components/Elevation/index.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import '@material/web/elevation/elevation'
+import { cn } from '@/packages/utils'
+import { cva } from 'class-variance-authority'
+
+const elevationStyles = cva(
+  'rounded-[inherit] absolute inset-0 before:absolute before:inset-0 before:transition-all after:absolute after:inset-0 after:transition-all before:rounded-[inherit] before:text-black before:opacity-30 after:opacity-[0.15] after:rounded-[inherit] after:text-black',
+  {
+    variants: {
+      level: {
+        0: 'before:shadow-[0_0_0_0] after:shadow-[0_0_0_0]',
+        1: 'before:shadow-[0_1px_2px_0] after:shadow-[0_1px_3px_1px]',
+        2: 'before:shadow-[0_1px_2px_0] after:shadow-[0_2px_6px_2px]',
+        3: 'before:shadow-[0_1px_3px_0] after:shadow-[0_4px_8px_3px]',
+        4: 'before:shadow-[0_2px_3px_0] after:shadow-[0_6px_10px_4px]',
+        5: 'before:shadow-[0_4px_4px_0] after:shadow-[0_8px_12px_6px]'
+      }
+    }
+  }
+)
 
 const Elevation = (props) => {
-  const { level, ...rest } = props
-  return <md-elevation style={{ '--md-elevation-level': level }} {...rest} />
+  const { level } = props
+  return <span className={cn(elevationStyles({ level }))}></span>
 }
 
 Elevation.propTypes = {
   level: PropTypes.oneOf([0, 1, 2, 3, 4, 5, '0', '1', '2', '3', '4', '5'])
-}
-
-Elevation.defaultProps = {
-  level: 1
 }
 
 export default Elevation
