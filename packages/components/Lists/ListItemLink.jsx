377e991a966bfca0de3c15ef0042f14a3f5e11b6
lerte smith <lerte@qq.com>
2023-08-06 22:19:19 +0800

    use NavLink

--- packages/components/Lists/ListItemLink.jsx
+++ packages/components/Lists/ListItemLink.jsx
@@ -1,14 +1,15 @@
 import React, { forwardRef } from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import '@material/web/list/list-item'
 
 const ListItemLink = forwardRef((props, ref) => {
   const { to, target, className, children, ...rest } = props
 
   return (
-    <Link to={to} target={target}>
-      <md-list-item ref={ref} class={className} {...rest} />
-    </Link>
+    <NavLink to={to} target={target} className={({ isActive }) => (isActive ? 'text-primary bg-black/10' : '')}>
+      {/* <md-list-item ref={ref} class={className} {...rest} /> */}
+      <div className="mb-1 pl-4 py-4 bg-[inherit] hover:bg-black/10 hover:dark:bg-white/10">{rest.headline}</div>
+    </NavLink>
   )
 })
 
