23092d11411b640f81f9e8dc014f54b63bce63b9
lerte smith <lerte@qq.com>
2023-08-08 18:59:33 +0800

    update card

--- src/docs/components/cards.md
+++ src/docs/components/cards.md
@@ -11,15 +11,18 @@ title: Cards
 <usage name="card"></usage>
 
 ```jsx
-import { Button, Card } from 'actify'
+import { Card } from 'actify'
 
 export default () => {
   return (
     <div className="flex flex-col">
-      <Card color="primary" variant="filled">
-        <p>filled primary card</p>
-        <div className="flex justify-end">
-          <Button variant="tonal">Get started</Button>
+      <Card className="max-w-xs">
+        <img className="rounded-t-lg" src="https://picsum.photos/480/240" atl="" />
+        <div class="block flex-grow flex-shrink p-5">
+          <div class="font-medium text-gray-700 mb-3">Card title</div>
+          <div class="text-gray-500">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </div>
         </div>
       </Card>
     </div>
--- src/usage/card.jsx
+++ src/usage/card.jsx
@@ -1,12 +1,15 @@
-import { Button, Card } from '@/packages/components'
+import { Card } from '@/packages/components'
 
 export default () => {
   return (
-    <div className="flex flex-col">
-      <Card color="primary" variant="filled">
-        <p>filled primary card</p>
-        <div className="flex justify-end">
-          <Button variant="tonal">Get started</Button>
+    <div className="flex flex-col not-prose">
+      <Card className="max-w-xs">
+        <img className="rounded-t-lg" src="https://picsum.photos/480/240" atl="" />
+        <div class="block flex-grow flex-shrink p-5">
+          <div class="font-medium text-gray-700 mb-3">Card title</div>
+          <div class="text-gray-500">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </div>
         </div>
       </Card>
     </div>
