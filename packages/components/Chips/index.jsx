a2e381520faad6d63cdbfb10ed7b898c0b4a1a12
lerte smith <lerte@qq.com>
2023-08-11 23:28:18 +0800

    Chips Component

--- packages/components/Chips/index.jsx
+++ packages/components/Chips/index.jsx
@@ -1,41 +1,63 @@
-import React from 'react'
 import PropTypes from 'prop-types'
-import { Icon } from 'actify'
-import '@material/web/chips/chip-set'
-import '@material/web/chips/assist-chip'
-import '@material/web/chips/filter-chip'
-import '@material/web/chips/input-chip'
-import '@material/web/chips/suggestion-chip'
+import { Icon, Ripple } from 'actify'
+import { tv } from 'tailwind-variants'
+import React, { useState, forwardRef } from 'react'
 
-const Chip = (props) => {
-  const { type, icon, className, children, ...rest } = props
+const variants = tv({
+  base: 'relative inline-flex items-center gap-2 px-4 h-8 rounded-lg text-sm border no-underline hover:bg-black/10 dark:hover:bg-white/10',
+  variants: {
+    type: {
+      assist: '',
+      filter: '',
+      input: '',
+      suggestion: ''
+    }
+  }
+})
+
+const Chip = forwardRef((props, ref) => {
+  const [show, setShow] = useState(true)
+  const [selected, setSelected] = useState(false)
+  const { ripple, label, type, color, icon, style, className, children, ...rest } = props
+
+  const handleClick = () => {
+    rest.onClick && rest.onClick()
+    if (type == 'filter') {
+      setSelected(!selected)
+    }
+  }
+  // assist and suggestion chip have href
+  const Tag = ['assist', 'suggestion'].includes(type) && rest.href ? 'a' : 'div'
 
   return (
-    <md-chip-set>
-      {type == 'assist' && (
-        <md-assist-chip {...rest}>
-          <span slot="icon">{icon && <Icon name={icon} />}</span>
-        </md-assist-chip>
-      )}
-      {type == 'filter' && (
-        <md-filter-chip {...rest}>
-          <span slot="icon">
-            <Icon name="Circle" size={18} />
-          </span>
-        </md-filter-chip>
-      )}
-      {type == 'input' && <md-input-chip {...rest} />}
-      {type == 'suggestion' && <md-suggestion-chip {...rest} />}
-    </md-chip-set>
+    show && (
+      <Tag ref={ref} {...rest} style={style} className={variants({ type, className })} onClick={handleClick}>
+        {type == 'filter' && <Icon name={`${selected ? 'Check' : 'Circle'}`} size={18} color={color} />}
+        {label}
+        {ripple && <Ripple />}
+        {type == 'input' && (
+          <Icon
+            name="X"
+            size={18}
+            onClick={() => setShow(false)}
+            className="hover:after:rounded-full hover:after:absolute hover:after:-inset-1 hover:after:bg-black/5 dark:hover:after:bg-white/5"
+          />
+        )}
+      </Tag>
+    )
   )
-}
+})
 
 Chip.propTypes = {
+  ripple: PropTypes.bool,
+  color: PropTypes.string,
   type: PropTypes.oneOf(['assist', 'filter', 'input', 'suggestion'])
 }
 
 Chip.defaultProps = {
-  type: 'assist'
+  ripple: true,
+  type: 'assist',
+  color: 'primary'
 }
 
 Chip.displayName = 'Actify.Chip'
--- src/usage/chips.jsx
+++ src/usage/chips.jsx
@@ -3,10 +3,10 @@ import { Chip } from 'actify'
 export default () => {
   return (
     <div className="flex gap-2">
-      <Chip label="Ramp access" />
+      <Chip label="Baidu" href="https://www.baidu.com" target="_blank" />
       <Chip type="filter" label="filter" />
       <Chip type="input" label="input" />
-      <Chip type="suggestion" label="Suggestion" />
+      <Chip type="suggestion" label="Actify" href="https://actifyjs.com" target="_blank" />
     </div>
   )
 }
