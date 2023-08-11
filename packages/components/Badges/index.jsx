37786bb74833ed04c4a44eddfdd09ef514880e21
lerte smith <lerte@qq.com>
2023-08-11 16:01:50 +0800

    Badge Component

--- packages/components/Badges/index.jsx
+++ packages/components/Badges/index.jsx
@@ -1,22 +1,32 @@
 import React, { forwardRef } from 'react'
 import PropTypes from 'prop-types'
-import '@material/web/labs/badge/badge'
+import { tv } from 'tailwind-variants'
 
-import { setColor } from '@/packages/utils'
+const variants = tv({
+  base: "absolute rounded-full p-1 text-xs font-medium content-[''] leading-none grid place-items-center top-[4%] right-[2%] translate-x-1/2 -translate-y-1/2 text-white min-w-[24px] min-h-[24px]",
+  variants: {
+    color: {
+      primary: 'bg-primary',
+      secondary: 'bg-secondary',
+      tertiary: 'bg-tertiary',
+      error: 'bg-error'
+    }
+  },
+  defaultVariants: {
+    color: 'error'
+  }
+})
 
 const Badge = forwardRef((props, ref) => {
   const { style, color, value, className, children, ...rest } = props
-  let styles = { ...style }
-  if (color) {
-    styles['--md-badge-color'] = setColor(color)
-    styles['--md-badge-large-color'] = setColor(color)
-  }
   const number = isNaN(parseInt(value)) ? '' : parseInt(value)
 
   return (
-    <div className="inline-flex relative">
+    <div className="inline-flex relative" ref={ref} {...rest}>
       {children}
-      <md-badge ref={ref} value={number > 999 ? '999+' : number} {...rest} class={className} style={{ ...styles }} />
+      <span className={variants({ color, className })} style={style}>
+        {number > 999 ? '999+' : number}
+      </span>
     </div>
   )
 })
