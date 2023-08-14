e77c2f0b63fb64c15ee5684d6afd66f3d22f9c78
lerte smith <lerte@qq.com>
2023-08-14 14:49:30 +0800

    LinearProgress Component

--- packages/components/LinearProgress/index.jsx
+++ packages/components/LinearProgress/index.jsx
@@ -1,14 +1,91 @@
+import PropTypes from 'prop-types'
+import { tv } from 'tailwind-variants'
 import React, { forwardRef } from 'react'
-import '@material/web/progress/linear-progress'
-import { setColor, removeFalseValue } from '@/packages/utils'
+import { setColor } from '@/packages/utils'
+
+const variants = tv({
+  base: 'h-1 relative overflow-hidden'
+})
+
+const barVariants = tv({
+  base: 'absolute animate-[auto_ease_0s_1_normal_none_running_none] transition-transform',
+  variants: {
+    indeterminate: {
+      primary: 'animate-[2s_linear_0s_infinite_normal_none_running_primary-indeterminate-translate]',
+      secondary: 'animate-[2s_linear_0s_infinite_normal_none_running_secondary-indeterminate-translate]'
+    }
+  }
+})
+
+const barInnerVariants = tv({
+  base: 'absolute inset-0 animate-[auto_ease_0s_1_normal_none_running_none] bg-[var(--color, #1976d2)]',
+  variants: {
+    indeterminate: {
+      primary: 'animate-[2s_linear_0s_infinite_normal_none_running_primary-indeterminate-scale]',
+      secondary: 'animate-[2s_linear_0s_infinite_normal_none_running_secondary-indeterminate-scale]'
+    }
+  }
+})
 
 const LinearProgress = forwardRef((props, ref) => {
-  const { style, color, className, ...rest } = props
-  const attributes = removeFalseValue(rest)
-  let styles = { ...style }
-  color && (styles['--md-linear-progress-active-indicator-color'] = setColor(color))
-  return <md-linear-progress {...attributes} ref={ref} style={{ ...styles }} />
+  const { value, indeterminate, style, color, className, ...rest } = props
+
+  return (
+    <div
+      ref={ref}
+      {...rest}
+      style={{ ...style, '--color': setColor(color) }}
+      className={variants({ indeterminate, className })}
+    >
+      <div
+        className="absolute inset-0"
+        style={{
+          transform: 'scaleX(1)',
+          transformOrigin: 'left center',
+          transition: 'transform 250ms cubic-bezier(0.4, 0, 0.6, 1) 0s',
+          backgroundColor: 'var(--md-sys-color-surface-container-highest, #e6e0e9)'
+        }}
+      />
+      <div
+        className={barVariants({ indeterminate: indeterminate ? 'primary' : null })}
+        style={{
+          blockSize: '100%',
+          inlineSize: '100%',
+          insetInlineStart: indeterminate ? '-145.167%' : 0,
+          backgroundColor: 'var(--color)',
+          transition: 'none 0s ease 0s',
+          transformOrigin: 'left center',
+          transform: `scaleX(${indeterminate ? 1 : value})`
+        }}
+      >
+        <div className={barInnerVariants({ indeterminate: indeterminate ? 'primary' : null })}></div>
+      </div>
+      <div
+        style={{
+          blockSize: '100%',
+          inlineSize: '100%',
+          transition: 'none 0s ease 0s',
+          backgroundColor: 'var(--color)',
+          transformOrigin: 'left center',
+          insetInlineStart: '-54.8889%'
+        }}
+        className={barVariants({ indeterminate: indeterminate ? 'secondary' : null })}
+      >
+        <div className={barInnerVariants({ indeterminate: indeterminate ? 'secondary' : null })}></div>
+      </div>
+    </div>
+  )
 })
+
+LinearProgress.propTypes = {
+  color: PropTypes.string,
+  indeterminate: PropTypes.bool
+}
+
+LinearProgress.defaultProps = {
+  color: 'primary',
+  indeterminate: false
+}
 
 LinearProgress.displayName = 'Actify.LinearProgress'
 
