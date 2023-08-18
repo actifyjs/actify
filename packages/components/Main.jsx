c38a268726f175d68012ee0c71ea09c935b659f4
lerte smith <lerte@qq.com>
2023-08-18 20:24:57 +0800

    responsive

--- packages/components/Main.jsx
+++ packages/components/Main.jsx
@@ -1,21 +1,26 @@
 import { forwardRef } from 'react'
+import { tv } from 'tailwind-variants'
 import { useApp } from './App/AppContext'
 import { useLocation } from 'react-router-dom'
+
+const variants = tv({
+  base: 'container mx-auto min-h-[calc(100vh-120px)] p-4 lg:pr-64 pl-[var(--left)]',
+  variants: {
+    index: {
+      true: '!pr-4'
+    }
+  }
+})
 
 const Main = forwardRef((props, ref) => {
   const { app } = useApp()
   const { pathname } = useLocation()
   const { children, className, ...rest } = props
-  const paddingLeft = pathname == '/' ? 0 : app.left
-  const paddingRight = pathname == '/' ? 0 : 256
+  const index = pathname == '/' ? true : false
+  const paddingLeft = index ? 0 : app.left + 'px'
 
   return (
-    <main
-      ref={ref}
-      {...rest}
-      style={{ paddingLeft, paddingRight }}
-      className="container mx-auto min-h-[calc(100vh-120px)] p-4"
-    >
+    <main ref={ref} {...rest} style={{ '--left': paddingLeft }} className={variants({ index, className })}>
       {children}
     </main>
   )
