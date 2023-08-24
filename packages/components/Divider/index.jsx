f7279528c430b690d88f4c6739fd13d544f02086
lerte smith <lerte@qq.com>
2023-08-24 18:56:24 +0800

    update divider

--- packages/components/Divider/index.jsx
+++ packages/components/Divider/index.jsx
@@ -3,20 +3,17 @@ import PropTypes from 'prop-types'
 import { tv } from 'tailwind-variants'
 import { setColor } from '@/packages/utils'
 
-const variants = tv(
-  'relative h-[var(--md-divider-thickness)] before:absolute before:inset-0 before:bg-[var(--md-divider-color)] before:h-[var(--md-divider-thickness)] before:w-full',
-  {
-    variants: {}
-  }
-)
+const variants = tv({
+  base: 'relative before:absolute before:inset-0 before:bg-[var(--color)]'
+})
 
 const Divider = (props) => {
   const { style, color, thickness, className, ...rest } = props
   const styles = { ...style }
   if (color) {
-    styles['--md-divider-color'] = setColor(color)
+    styles['--color'] = setColor(color)
   } else {
-    styles['--md-divider-color'] = 'var(--md-sys-color-outline-variant, #cac4d0)'
+    styles['--color'] = 'var(--md-sys-color-outline-variant, #cac4d0)'
   }
   if (thickness) {
     styles['height'] = thickness + 'px'
@@ -24,12 +21,14 @@ const Divider = (props) => {
     styles['height'] = '1px'
   }
 
-  return <div {...rest} style={styles} className={variants({ className })}></div>
+  return (
+    <div {...rest} style={styles} className={variants({ className })}></div>
+  )
 }
 
 Divider.propTypes = {
   color: PropTypes.string,
-  thickness: PropTypes.oneOf([PropTypes.number, PropTypes.string])
+  thickness: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
 }
 
 Divider.displayName = 'Actify.Divider'
