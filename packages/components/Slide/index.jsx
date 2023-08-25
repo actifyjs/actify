22204c266a1037b80bc546c9766f990a06f22466
lerte smith <lerte@qq.com>
2023-08-25 17:22:44 +0800

    slide component

---
+++ packages/components/Slide/index.jsx
@@ -0,0 +1,102 @@
+import { wrap } from 'popmotion'
+import React, { useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+
+const images = [
+  'https://d33wubrfki0l68.cloudfront.net/dd23708ebc4053551bb33e18b7174e73b6e1710b/dea24/static/images/wallpapers/shared-colors@2x.png',
+  'https://d33wubrfki0l68.cloudfront.net/49de349d12db851952c5556f3c637ca772745316/cfc56/static/images/wallpapers/bridge-02@2x.png',
+  'https://d33wubrfki0l68.cloudfront.net/594de66469079c21fc54c14db0591305a1198dd6/3f4b1/static/images/wallpapers/bridge-01@2x.png'
+]
+
+const variants = {
+  enter: (direction) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0
+    }
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1
+  },
+  exit: (direction) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0
+    }
+  }
+}
+
+/**
+ * Experimenting with distilling swipe offset and velocity into a single variable, so the
+ * less distance a user has swiped, the more velocity they need to register as a swipe.
+ * Should accomodate longer swipes and short flicks without having binary checks on
+ * just distance thresholds and velocity > 0.
+ */
+const swipeConfidenceThreshold = 10000
+const swipePower = (offset, velocity) => {
+  return Math.abs(offset) * velocity
+}
+
+const Slide = () => {
+  const [[page, direction], setPage] = useState([0, 0])
+
+  // We only have 3 images, but we paginate them absolutely (ie 1, 2, 3, 4, 5...) and
+  // then wrap that within 0-2 to find our image ID in the array below. By passing an
+  // absolute page index as the `motion` component's `key` prop, `AnimatePresence` will
+  // detect it as an entirely new image. So you can infinitely paginate as few as 1 images.
+  const imageIndex = wrap(0, images.length, page)
+
+  const paginate = (newDirection) => {
+    setPage([page + newDirection, newDirection])
+  }
+
+  return (
+    <>
+      <AnimatePresence initial={false} custom={direction}>
+        <motion.img
+          className="w-full h-full absolute object-cover"
+          key={page}
+          src={images[imageIndex]}
+          custom={direction}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { type: 'spring', stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 }
+          }}
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          dragElastic={1}
+          onDragEnd={(e, { offset, velocity }) => {
+            const swipe = swipePower(offset.x, velocity.x)
+
+            if (swipe < -swipeConfidenceThreshold) {
+              paginate(1)
+            } else if (swipe > swipeConfidenceThreshold) {
+              paginate(-1)
+            }
+          }}
+        />
+      </AnimatePresence>
+      <div
+        onClick={() => paginate(1)}
+        className="z-10 select-none right-3 top-[calc(50%-20px)] absolute bg-white rounded-full w-[40px] h-[40px] flex justify-center items-center cursor-pointer text-[18px] font-bold text-black"
+      >
+        ‣
+      </div>
+      <div
+        onClick={() => paginate(-1)}
+        className="z-10 select-none left-3 top-[calc(50%-20px)] absolute rotate-180 bg-white rounded-full w-[40px] h-[40px] flex justify-center items-center cursor-pointer text-[18px] font-bold text-black"
+      >
+        ‣
+      </div>
+    </>
+  )
+}
+
+export default Slide
---
+++ src/pages/slide.jsx
@@ -0,0 +1,9 @@
+import Slide from '@/packages/components/Slide'
+
+export default () => {
+  return (
+    <div className="w-full h-[600px] rounded-lg relative overflow-hidden flex items-center justify-center">
+      <Slide />
+    </div>
+  )
+}
