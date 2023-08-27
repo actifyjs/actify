d982c47a60a66fa2d6b1ce491f40eb3846f40a2e
lerte smith <lerte@qq.com>
2023-08-27 22:48:35 +0800

    ListItem add animation

--- packages/components/Lists/ListItem.jsx
+++ packages/components/Lists/ListItem.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import { Ripple } from 'actify'
+import { motion } from 'framer-motion'
 import { tv } from 'tailwind-variants'
 
 const variants = tv({
-  base: 'relative flex items-center h-14 pl-4 leading-normal cursor-pointer hover:bg-secondary'
+  base: 'relative flex items-center h-14 pl-4 leading-normal cursor-pointer isolate'
 })
 
 const ListItem = React.forwardRef((props, ref) => {
   const { className, children, ...rest } = props
+  const [current, setCurrent] = React.useState()
 
   return (
-    <li ref={ref} className={variants({ className })} {...rest}>
+    <li
+      ref={ref}
+      {...rest}
+      className={variants({ className })}
+      onMouseOver={() => setCurrent(children)}
+      onMouseOut={() => setCurrent(undefined)}
+    >
       {children}
+      {children == current && (
+        <motion.div
+          layoutId="actify-moving"
+          className="absolute inset-0 bg-secondary/25 z-[-1]"
+        />
+      )}
       <Ripple />
     </li>
   )
