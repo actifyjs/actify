828cd04e995cdb6496c49ae74d60155d0299322b
lerte smith <lerte@qq.com>
2023-08-27 22:21:45 +0800

    update ListItemLink

--- packages/components/Lists/ListItemLink.jsx
+++ packages/components/Lists/ListItemLink.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import '@material/web/list/list-item'
 import { NavLink } from 'react-router-dom'
 
 const ListItemLink = React.forwardRef((props, ref) => {
   const { to, target, className, children, ...rest } = props
 
   return (
-    <NavLink to={to} target={target} className={({ isActive }) => (isActive ? 'text-primary bg-black/10' : '')}>
-      {/* <md-list-item ref={ref} class={className} {...rest} /> */}
-      <div className="mb-1 pl-4 py-4 bg-[inherit] hover:bg-black/10 hover:dark:bg-white/10">{rest.headline}</div>
+    <NavLink
+      to={to}
+      target={target}
+      className={({ isActive }) => (isActive ? 'text-primary bg-black/10' : '')}
+    >
+      <div className="mb-1 pl-4 py-4 bg-[inherit] hover:bg-black/10 hover:dark:bg-white/10">
+        {rest.headline}
+      </div>
     </NavLink>
   )
 })
