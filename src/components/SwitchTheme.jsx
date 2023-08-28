cc080a3d43db4a97b32d6d01f73283a8d78fbd0b
lerte smith <lerte@qq.com>
2023-08-28 20:56:37 +0800

    update

--- src/components/SwitchTheme.jsx
+++ src/components/SwitchTheme.jsx
@@ -9,7 +9,8 @@ const SwitchTheme = forwardRef((props, ref) => {
   const initTheme = () => {
     if (
       localStorage.theme === 'dark' ||
-      (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
     ) {
       setMode('dark')
       document.documentElement.classList.add('dark')
@@ -36,7 +37,8 @@ const SwitchTheme = forwardRef((props, ref) => {
 
   const toggleTheme = (event) => {
     const isAppearanceTransition =
-      document.startViewTransition && !window.matchMedia('(prefers-reduced-motion: reduce)').matches
+      document.startViewTransition &&
+      !window.matchMedia('(prefers-reduced-motion: reduce)').matches
 
     if (!isAppearanceTransition) {
       toggleMode()
@@ -45,12 +47,18 @@ const SwitchTheme = forwardRef((props, ref) => {
 
     const x = event.clientX
     const y = event.clientY
-    const endRadius = Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y))
+    const endRadius = Math.hypot(
+      Math.max(x, innerWidth - x),
+      Math.max(y, innerHeight - y)
+    )
     const transition = document.startViewTransition(() => {
       toggleMode()
     })
     transition.ready.then(() => {
-      const clipPath = [`circle(0px at ${x}px ${y}px)`, `circle(${endRadius}px at ${x}px ${y}px)`]
+      const clipPath = [
+        `circle(0px at ${x}px ${y}px)`,
+        `circle(${endRadius}px at ${x}px ${y}px)`
+      ]
       document.documentElement.animate(
         {
           clipPath: mode == 'dark' ? [...clipPath].reverse() : clipPath
@@ -58,14 +66,22 @@ const SwitchTheme = forwardRef((props, ref) => {
         {
           duration: 500,
           easing: 'ease-out',
-          pseudoElement: mode == 'dark' ? '::view-transition-old(root)' : '::view-transition-new(root)'
+          pseudoElement:
+            mode == 'dark'
+              ? '::view-transition-old(root)'
+              : '::view-transition-new(root)'
         }
       )
     })
   }
 
   return (
-    <div ref={ref} {...rest} onClick={toggleTheme} className={twMerge('cursor-pointer overflow-hidden', className)}>
+    <div
+      ref={ref}
+      {...rest}
+      onClick={toggleTheme}
+      className={twMerge('cursor-pointer overflow-hidden', className)}
+    >
       <div className="grid place-content-center">
         {mode == 'dark' ? (
           <IconButton>
@@ -73,7 +89,7 @@ const SwitchTheme = forwardRef((props, ref) => {
           </IconButton>
         ) : (
           <IconButton>
-            <Icon name="Moon" color="primary" />
+            <Icon name="MoonStar" color="primary" />
           </IconButton>
         )}
       </div>
--- src/pages/examples/index.jsx
+++ src/pages/examples/index.jsx
@@ -1,13 +1,16 @@
-import Search from '@/packages/components/Search'
-import TopAppBar from '@/packages/components/TopAppBar'
-import BottomAppBar from '@/packages/components/BottomAppBar'
-
 export default () => {
   return (
-    <div className="flex flex-col gap-2">
-      <TopAppBar title="TopAppBar" />
-      <Search placeholder="search here" />
-      <BottomAppBar />
+    <div>
+      <ul className="w-full grid gap-2 grid-cols-[repeat(auto-fill,minmax(160px,1fr))]">
+        {[...Array(10)].map((_, i) => (
+          <li
+            key={i}
+            className="h-28 cursor-pointer rounded-md bg-secondary text-on-secondary grid place-content-center"
+          >
+            item {i + 1}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
