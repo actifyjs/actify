8d4a81ed61b4cc7abb5894cf70431e0ca36e9d0d
lerte smith <lerte@qq.com>
2023-10-11 01:10:10 +0800

    update alt

--- src/usage/carousel.jsx
+++ src/usage/carousel.jsx
@@ -6,21 +6,21 @@ export default () => {
       <Carousel autoPlay control indicator infinite>
         <Carousel.Item>
           <img
-            alt="image 1"
+            alt="carousel 1"
             className="h-full w-full object-cover"
             src="https://images.unsplash.com/photo-1692812298405-db9c1ca093ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80"
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
-            alt="image 2"
+            alt="carousel 2"
             className="h-full w-full object-cover"
             src="https://images.unsplash.com/photo-1692902295604-6c5109aa55a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80"
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
-            alt="image 3"
+            alt="carousel 3"
             className="h-full w-full object-cover"
             src="https://images.unsplash.com/photo-1692589250460-1ea229c9942a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
           />
