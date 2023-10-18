feb469d54cb11051bd141ac7960bd25311f8a347
lerte smith <lerte@qq.com>
2023-10-18 17:22:00 +0800

    Before After Component

---
+++ src/docs/components/before-after.md
@@ -0,0 +1,11 @@
+---
+title: Before After
+---
+
+# Before After
+
+> Before after show a before and after image of a product.
+
+## Usage
+
+<usage name="before-after"></usage>
---
+++ src/usage/before-after.jsx
@@ -0,0 +1,10 @@
+import { BeforeAfter } from 'actify'
+
+export default () => {
+  return (
+    <BeforeAfter
+      before="https://cdn.mpfast.cn/FqF34OzNX0xkVq0Pj-zE6kwpk4qN"
+      after="https://cdn.mpfast.cn/52c51e6f-f712-4cdf-8151-01ad49a5f3b4.png"
+    />
+  )
+}
