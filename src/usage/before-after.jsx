339352bfd145603c030313d7a13af09e755f46fc
lerte smith <lerte@qq.com>
2023-10-19 21:48:36 +0800

    update before after component

--- src/usage/before-after.jsx
+++ src/usage/before-after.jsx
@@ -1,10 +1,5 @@
 import { BeforeAfter } from 'actify'
 
 export default () => {
-  return (
-    <BeforeAfter
-      before="https://cdn.mpfast.cn/FqF34OzNX0xkVq0Pj-zE6kwpk4qN"
-      after="https://cdn.mpfast.cn/52c51e6f-f712-4cdf-8151-01ad49a5f3b4.png"
-    />
-  )
+  return <BeforeAfter before="/images/before.jpg" after="/images/after.png" />
 }
