360bdb310c2ceb2a9b7d13dcbfb5a63d9dcf83cd
lerte smith <lerte@qq.com>
2023-10-19 21:54:30 +0800

    fix CodePreview

--- src/components/CodePreview.jsx
+++ src/components/CodePreview.jsx
@@ -5,30 +5,33 @@ import { LiveProvider, LiveEditor, LivePreview } from 'react-live'
 const CodePreview = ({ code }) => {
   return (
     <LiveProvider code={code} scope={Actify}>
-      <Tabs value="preview" className="not-prose rounded-lg bg-secondary/10">
+      <Actify.Tabs
+        value="preview"
+        className="not-prose rounded-lg bg-secondary/10"
+      >
         <div className="flex border-b border-[#ccc] dark:border-[#222]">
-          <Tabs.Header className="bg-transparent">
-            <Tabs.Tab value="preview" className="min-w-[120px]">
-              <Icon name="eye" size={18} />
+          <Actify.Tabs.Header className="bg-transparent">
+            <Actify.Tabs.Tab value="preview" className="min-w-[120px]">
+              <Actify.Icon name="eye" size={18} />
               Preview
-            </Tabs.Tab>
-            <Tabs.Tab value="code" className="min-w-[120px]">
-              <Icon name="code-2" size={18} />
+            </Actify.Tabs.Tab>
+            <Actify.Tabs.Tab value="code" className="min-w-[120px]">
+              <Actify.Icon name="code-2" size={18} />
               Code
-            </Tabs.Tab>
-          </Tabs.Header>
+            </Actify.Tabs.Tab>
+          </Actify.Tabs.Header>
         </div>
-        <Tabs.Body className="p-2">
-          <Tabs.Panel value="preview">
+        <Actify.Tabs.Body className="p-2">
+          <Actify.Tabs.Panel value="preview">
             <LivePreview className="flex gap-2" />
-          </Tabs.Panel>
-          <Tabs.Panel value="code">
+          </Actify.Tabs.Panel>
+          <Actify.Tabs.Panel value="code">
             <SyntaxHighlighter language="jsx">
               <LiveEditor />
             </SyntaxHighlighter>
-          </Tabs.Panel>
-        </Tabs.Body>
-      </Tabs>
+          </Actify.Tabs.Panel>
+        </Actify.Tabs.Body>
+      </Actify.Tabs>
     </LiveProvider>
   )
 }
