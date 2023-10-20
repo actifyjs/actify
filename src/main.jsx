3590f410f00545ccbd17688d90465bc599ba676f
lerte smith <lerte@qq.com>
2023-10-20 20:27:44 +0800

    custom suspense fallback loading

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,19 @@ import Layout from './layout'
 import routes from '~react-pages'
 import ReactDOM from 'react-dom/client'
 import { StrictMode, Suspense } from 'react'
+import { LinearProgress } from 'actify'
 import ToastContainer from '@/packages/components/Toast/ToastContainer'
 import { ToastProvider } from '@/packages/components/Toast/ToastContext'
 import { BrowserRouter, useRoutes } from 'react-router-dom'
 
+const Loading = () => (
+  <div className="fixed z-50 w-full top-0 left-0">
+    <LinearProgress indeterminate />
+  </div>
+)
+
 const App = () => (
-  <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>
+  <Suspense fallback={<Loading />}>{useRoutes(routes)}</Suspense>
 )
 
 ReactDOM.createRoot(document.getElementById('app')).render(
