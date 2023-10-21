e885c26ccbb6d3b2cb2df352a2def27da068fa23
lerte smith <lerte@qq.com>
2023-10-21 21:57:56 +0800

    update fake loading

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,40 @@ import './main.css'
 import Layout from './layout'
 import routes from '~react-pages'
 import ReactDOM from 'react-dom/client'
-import { StrictMode, Suspense } from 'react'
+import { useInterval } from 'usehooks-ts'
 import { Analytics } from '@vercel/analytics/react'
+import { StrictMode, Suspense, useState, useEffect } from 'react'
 import { BrowserRouter, useRoutes } from 'react-router-dom'
 import { LinearProgress, ToastProvider, ToastContainer } from 'actify'
 
-const Loading = () => (
-  <div className="fixed z-50 w-full top-0 left-0">
-    <LinearProgress indeterminate />
-  </div>
-)
+const Loading = () => {
+  const [step, setStep] = useState(0.5)
+  const [progress, setProgress] = useState(0)
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    return () => {
+      setIsLoading(false)
+    }
+  }, [])
+
+  useInterval(
+    () => {
+      setProgress((_) => _ + step)
+      if (progress >= 100) {
+      } else if (progress >= 70) {
+        setStep(0.1)
+      }
+    },
+    isLoading ? 100 : null
+  )
+
+  return (
+    <div className="fixed z-50 w-full top-0 left-0">
+      <LinearProgress value={progress} />
+    </div>
+  )
+}
 
 const App = () => (
   <Suspense fallback={<Loading />}>{useRoutes(routes)}</Suspense>
