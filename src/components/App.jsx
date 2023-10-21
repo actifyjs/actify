bb5519fa38aa910b7795cf635fb1c0722e28adde
lerte smith <lerte@qq.com>
2023-10-21 11:30:12 +0800

    optimize

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ const App = ({ children }) => {
     <AppProvider>
       <div
         id="actify"
-        className="min-h-screen grid grid-cols-1 gap-4 grid-rows-[64px_auto_56px] bg-surface text-on-surface"
+        className="min-h-screen grid grid-cols-1 gap-4 grid-rows-[64px_auto_56px] bg-surface"
       >
         {children}
       </div>
