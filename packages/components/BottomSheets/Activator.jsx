831cd2b2aeceab5479e222f5a3d81b39b7f42bb1
lerte smith <lerte@qq.com>
2023-11-05 22:15:33 +0800

    Activator add asChild props

--- packages/components/BottomSheets/Activator.jsx
+++ packages/components/BottomSheets/Activator.jsx
@@ -1,19 +1,30 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, isValidElement, cloneElement } from 'react'
 import { useBottomSheets } from './Context'
 
-const Activator = forwardRef(({ className, children, ...rest }, ref) => {
-  const { open, setOpen } = useBottomSheets()
+const Activator = forwardRef(
+  ({ asChild = false, className, children, ...rest }, ref) => {
+    const { open, setOpen } = useBottomSheets()
 
-  return (
-    <div
-      {...rest}
-      ref={ref}
-      className={className}
-      onClick={() => setOpen(!open)}
-    >
-      {children}
-    </div>
-  )
-})
+    // `asChild` allows the user to pass any element as the activator
+    if (asChild && isValidElement(children)) {
+      return cloneElement(children, {
+        ref,
+        ...rest,
+        ...children.props,
+        onClick: () => setOpen(!open)
+      })
+    }
+    return (
+      <div
+        {...rest}
+        ref={ref}
+        className={className}
+        onClick={() => setOpen(!open)}
+      >
+        {children}
+      </div>
+    )
+  }
+)
 
 export { Activator }
--- packages/components/SideSheets/Activator.jsx
+++ packages/components/SideSheets/Activator.jsx
@@ -1,19 +1,31 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, isValidElement, cloneElement } from 'react'
 import { useSideSheets } from './Context'
 
-const Activator = forwardRef(({ className, children, ...rest }, ref) => {
-  const { open, setOpen } = useSideSheets()
+const Activator = forwardRef(
+  ({ asChild, className, children, ...rest }, ref) => {
+    const { open, setOpen } = useSideSheets()
 
-  return (
-    <div
-      {...rest}
-      ref={ref}
-      className={className}
-      onClick={() => setOpen(!open)}
-    >
-      {children}
-    </div>
-  )
-})
+    // `asChild` allows the user to pass any element as the activator
+    if (asChild && isValidElement(children)) {
+      return cloneElement(children, {
+        ref,
+        ...rest,
+        ...children.props,
+        onClick: () => setOpen(!open)
+      })
+    }
+
+    return (
+      <div
+        {...rest}
+        ref={ref}
+        className={className}
+        onClick={() => setOpen(!open)}
+      >
+        {children}
+      </div>
+    )
+  }
+)
 
 export { Activator }
