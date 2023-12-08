d1e94a0b22729686f18b144022a8c3774b551093
lerte smith <lerte@qq.com>
2023-12-09 02:01:47 +0800

    :rocket:perf: add pwa icons

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -1,18 +1,87 @@
 import { defineConfig } from 'vite'
 import pages from 'vite-plugin-pages'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import mdPlugin, { Mode } from 'vite-plugin-markdown'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: 'prompt',
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+  manifest: {
+    name: 'Actify',
+    short_name: 'Actify',
+    description: '🌻React Tailwind CSS Material Design 3 Components Library',
+    theme_color: '#006a6a',
+    background_color: '#ffffff',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    icons: [
+      {
+        src: '/icons/icon-48x48.png',
+        sizes: '48x48',
+        type: 'image/png',
+        purpose: 'maskable any'
+      },
+      {
+        src: '/icons/icon-72x72.png',
+        sizes: '72x72',
+        type: 'image/png',
+        purpose: 'maskable any'
+      },
+      {
+        src: '/icons/icon-96x96.png',
+        sizes: '96x96',
+        type: 'image/png',
+        purpose: 'maskable any'
+      },
+      {
+        src: '/icons/icon-128x128.png',
+        sizes: '128x128',
+        type: 'image/png',
+        purpose: 'maskable any'
+      },
+      {
+        src: '/icons/icon-144x144.png',
+        sizes: '144x144',
+        type: 'image/png',
+        purpose: 'maskable any'
+      },
+      {
+        src: '/icons/icon-152x152.png',
+        sizes: '152x152',
+        type: 'image/png',
+        purpose: 'maskable any'
+      },
+      {
+        src: '/icons/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable any'
+      },
+      {
+        src: '/icons/icon-384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+        purpose: 'maskable any'
+      },
+      {
+        src: '/icons/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable any'
+      }
+    ]
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     pages(),
-    VitePWA({
-      registerType: 'autoUpdate'
-    }),
+    VitePWA(manifestForPlugin),
     tsconfigPaths(),
     mdPlugin({ mode: [Mode.MARKDOWN] })
   ]
