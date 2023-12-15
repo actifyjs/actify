b6662e09ad1c27ec8d14aba9a5b150ff7d04ca9a
lerte smith <lerte@qq.com>
2023-12-15 18:41:57 +0800

    :book:docs: fix EditOnGithub link

--- apps/docs/src/components/EditOnGitHub.tsx
+++ apps/docs/src/components/EditOnGitHub.tsx
@@ -1,12 +1,12 @@
 import { Button } from 'actify'
 import { PenSquare } from 'lucide-react'
 
-const EditOnGitHub = ({ pathname }) => {
+const EditOnGitHub = ({ pathname }: { pathname: string }) => {
   return (
     <Button
       variant="text"
       target="_blank"
-      href={`https://github.com/actifyjs/actify/blob/main/src/docs${pathname}.md`}
+      href={`https://github.com/actifyjs/actify/blob/main/apps/docs/src/docs${pathname}.md`}
     >
       Edit this page on GitHub
       <PenSquare name="pen-square" size={18} />
