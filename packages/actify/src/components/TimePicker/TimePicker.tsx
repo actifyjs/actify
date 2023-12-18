810edead32526f2d7441fc28af2b4ed9c0b019bb
lerte smith <lerte@qq.com>
2023-12-19 00:06:35 +0800

    :rocket:feat:TimePicker

--- packages/actify/src/components/TimePicker/TimePicker.tsx
+++ packages/actify/src/components/TimePicker/TimePicker.tsx
@@ -1,105 +1,112 @@
-// 'use client'
-// import React, { useState, useEffect } from 'react'
+'use client'
+import React, { useState, useEffect } from 'react'
 
-// import {
-//   flip,
-//   useId,
-//   shift,
-//   offset,
-//   useRole,
-//   useClick,
-//   useDismiss,
-//   autoUpdate,
-//   useFloating,
-//   FloatingPortal,
-//   useInteractions,
-//   FloatingFocusManager
-// } from '@floating-ui/react'
+import {
+  flip,
+  useId,
+  shift,
+  offset,
+  useRole,
+  useClick,
+  Placement,
+  useDismiss,
+  autoUpdate,
+  useFloating,
+  FloatingPortal,
+  useInteractions,
+  FloatingFocusManager
+} from '@floating-ui/react'
 
-// import dayjs from 'dayjs'
-// import { Picker } from './Picker'
-// import { CalendarClock } from 'lucide-react'
-// import { TextField } from '@actify/TextFields'
-// import { IconButton } from '@actify/Button/IconButton'
+import dayjs from 'dayjs'
+import { TimeClock } from './TimeClock'
+import { CalendarClock } from 'lucide-react'
+import { TextField } from '@actify/TextFields'
+import { IconButton } from '@actify/Button/IconButton'
 
-// const TimePicker = ({ onChange }) => {
-//   const [open, setOpen] = useState(false)
-//   const [placement, setPlacement] = useState(null)
-//   const buttonId = useId()
-//   const [value, setValue] = useState(dayjs().format('HH:mm'))
+interface TimeValue extends dayjs.Dayjs {}
 
-//   const {
-//     refs,
-//     floatingStyles,
-//     context,
-//     placement: resultantPlacement
-//   } = useFloating({
-//     placement: placement ?? 'bottom-start',
-//     open,
-//     onOpenChange: setOpen,
-//     middleware: [offset(16), flip(), shift()],
-//     whileElementsMounted: autoUpdate
-//   })
+const TimePicker = ({
+  onChange
+}: {
+  onChange: ({ hour, minute }: { hour: number; minute: number }) => void
+}) => {
+  const [open, setOpen] = useState(false)
+  const [placement, setPlacement] = useState<Placement | null>(null)
+  const buttonId = useId()
+  const [value, setValue] = useState<TimeValue | undefined>(dayjs())
 
-//   // Handles opening the floating element via the Choose Emoji button.
-//   const { getReferenceProps, getFloatingProps } = useInteractions([
-//     useClick(context),
-//     useDismiss(context),
-//     useRole(context)
-//   ])
+  const {
+    refs,
+    floatingStyles,
+    context,
+    placement: resultantPlacement
+  } = useFloating({
+    placement: placement ?? 'bottom-start',
+    open,
+    onOpenChange: setOpen,
+    middleware: [offset(16), flip(), shift()],
+    whileElementsMounted: autoUpdate
+  })
 
-//   useEffect(() => {
-//     if (open) {
-//       setPlacement(resultantPlacement)
-//     } else {
-//       setPlacement(null)
-//     }
-//   }, [open, resultantPlacement])
+  // Handles opening the floating element via the Choose Emoji button.
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    useClick(context),
+    useDismiss(context),
+    useRole(context)
+  ])
 
-//   // Prevent input losing focus on Firefox VoiceOver
-//   const { 'aria-activedescendant': ignoreAria, ...floatingProps } =
-//     getFloatingProps()
+  useEffect(() => {
+    if (open) {
+      setPlacement(resultantPlacement)
+    } else {
+      setPlacement(null)
+    }
+  }, [open, resultantPlacement])
 
-//   const handleChange = (time) => {
-//     onChange?.(time)
-//     setValue(time.hour + ':' + time.minute)
-//     setOpen(false)
-//   }
+  // Prevent input losing focus on Firefox VoiceOver
+  const { 'aria-activedescendant': ignoreAria, ...floatingProps } =
+    getFloatingProps()
 
-//   return (
-//     <>
-//       <TextField
-//         id={buttonId}
-//         value={value}
-//         //@ts-ignore
-//         label="select time"
-//         ref={refs.setReference}
-//         {...getReferenceProps()}
-//       >
-//         <TextField.TrailingIcon>
-//           <IconButton>
-//             <CalendarClock />
-//           </IconButton>
-//         </TextField.TrailingIcon>
-//       </TextField>
-//       <FloatingPortal>
-//         {open && (
-//           <FloatingFocusManager context={context} modal={false}>
-//             <div
-//               ref={refs.setFloating}
-//               style={{ zIndex: 50, ...floatingStyles }}
-//               aria-labelledby={buttonId}
-//               {...floatingProps}
-//             >
-//               <Picker value={value} onChange={handleChange} />
-//             </div>
-//           </FloatingFocusManager>
-//         )}
-//       </FloatingPortal>
-//     </>
-//   )
-// }
+  const handleChange = ({ hour, minute }: { hour: number; minute: number }) => {
+    onChange?.({
+      hour,
+      minute
+    })
+    setValue(dayjs(value).hour(hour).minute(minute))
+    setOpen(false)
+  }
 
-export default function TimePicker() {
-  return <div> todo</div>
+  return (
+    <>
+      <TextField
+        id={buttonId}
+        value={dayjs(value).format('HH:mm')}
+        label="select time"
+        ref={refs.setReference}
+        {...getReferenceProps()}
+      >
+        <TextField.TrailingIcon>
+          <IconButton>
+            <CalendarClock />
+          </IconButton>
+        </TextField.TrailingIcon>
+      </TextField>
+      <FloatingPortal>
+        {open && (
+          <FloatingFocusManager context={context} modal={false}>
+            <div
+              ref={refs.setFloating}
+              style={{ zIndex: 50, ...floatingStyles }}
+              aria-labelledby={buttonId}
+              {...floatingProps}
+            >
+              <TimeClock value={value} onChange={handleChange} />
+            </div>
+          </FloatingFocusManager>
+        )}
+      </FloatingPortal>
+    </>
+  )
 }
+
+export default TimePicker
