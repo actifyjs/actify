aafe050007941b060cb6a33cf2d6031548cd9026
lerte smith <lerte@qq.com>
2023-12-25 02:08:02 +0800

    update tailwind.config.ts

--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -1,32 +1,20 @@
-// @ts-nocheck
-import type { Config } from 'tailwindcss'
-import colors from 'tailwindcss/colors'
 import typography from '@tailwindcss/typography'
 import { withMaterialColors } from 'tailwind-material-colors'
-
-delete colors['lightBlue']
-delete colors['warmGray']
-delete colors['trueGray']
-delete colors['coolGray']
-delete colors['blueGray']
 
 export default withMaterialColors(
   {
     darkMode: 'class',
     content: [
       './src/**/*.{html,md,js,ts,jsx,tsx}',
-      '../../packages/actify/**/*.{js,ts,jsx,tsx}',
       './node_modules/actify/dist/**/*.{js,ts,jsx,tsx}'
     ],
-    plugins: [typography],
-    theme: {
-      extend: {
-        colors: { ...colors }
-      }
-    }
+    plugins: [typography]
   },
   {
     primary: '#006a6a'
     // primary: '#5b6400'
+  },
+  {
+    extend: true
   }
-) satisfies Config
+)
