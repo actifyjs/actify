4c134f9f5daf23bae4dbbd050b76c8f3e0b4bafa
lerte smith <lerte@qq.com>
2024-01-03 19:41:47 +0800

    add cross-env

--- scripts/modify.js
+++ scripts/modify.js
@@ -1,13 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
-const currentDirectory = __dirname
-
-const packageJsonPath = path.join(
-  currentDirectory,
-  '../packages/actify/package.json'
-)
-console.log(555, currentDirectory, packageJsonPath)
+const packageJsonPath = path.join(__dirname, '../packages/actify/package.json')
 
 const packageJsonContent = fs.readFileSync(packageJsonPath, 'utf8')
 const packageJson = JSON.parse(packageJsonContent)
