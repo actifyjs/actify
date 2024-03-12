2b7230d747d03172d4f27a7db9f16d0e2297bf3a
Lerte Smith <lerte@qq.com>
2024-03-12 21:46:36 +0800

    :book:docs:update checkbox usage

--- apps/docs/src/usage/checkbox.tsx
+++ apps/docs/src/usage/checkbox.tsx
@@ -1,28 +1,67 @@
 import { Checkbox } from 'actify'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default () => {
-  const [selected, setSelected] = useState(false)
-  const [indeterminate, setIndeterminate] = useState(true)
+  const [checkedAll, setCheckedAll] = useState(false)
+  const [checked, setChecked] = useState([false, false, false])
+  const [indeterminate, setIndeterminate] = useState(false)
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSelected(event.target.checked)
+  const handleCheckedAll = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCheckedAll(event.target.checked)
     if (indeterminate == true) {
       setIndeterminate(false)
     }
+    if (event.target.checked) {
+      setChecked([true, true, true])
+    } else {
+      setChecked([false, false, false])
+    }
   }
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    checked[index] = e.target.checked
+    setChecked([...checked])
+  }
+
+  useEffect(() => {
+    if (checked.some((item) => item == true)) {
+      setIndeterminate(true)
+    } else {
+      setIndeterminate(false)
+    }
+    if (checked.every((item) => item == true)) {
+      setCheckedAll(true)
+    } else {
+      setCheckedAll(false)
+    }
+  }, [checked])
 
   return (
     <div className="flex flex-wrap gap-4">
       <Checkbox
-        checked={selected}
+        checked={checkedAll}
         indeterminate={indeterminate}
-        onChange={handleChange}
+        onChange={handleCheckedAll}
       />
-      <Checkbox color="secondary" />
-      <Checkbox color="tertiary" />
-      <Checkbox color="error" />
-      <Checkbox disabled color="error" />
+      <Checkbox
+        checked={checked[0]}
+        onChange={(e) => handleChange(e, 0)}
+        color="secondary"
+      />
+      <Checkbox
+        checked={checked[1]}
+        onChange={(e) => handleChange(e, 1)}
+        color="tertiary"
+      />
+      <Checkbox
+        checked={checked[2]}
+        onChange={(e) => handleChange(e, 2)}
+        color="error"
+      />
+      <Checkbox disabled />
     </div>
   )
 }
