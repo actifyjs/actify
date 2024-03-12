9f2b1e7234d06b13cb34855cde1b4ddb5fc4e36f
lerte smith <lerte@qq.com>
2024-03-12 16:34:53 +0800

    :book: docs:update radio button

--- apps/docs/src/pages/examples/all.tsx
+++ apps/docs/src/pages/examples/all.tsx
@@ -94,26 +94,23 @@ export default () => {
         </label>
       </div>
       <div className="grid grid-cols-[repeat(auto-fill,minmax(160px,1fr))] gap-2">
-        <label className="flex items-center gap-2">
-          <Radio name="actify" value="vue" />
-          <span>primary</span>
-        </label>
-        <label className="flex items-center gap-2">
-          <Radio name="actify" value="react" color="secondary" />
-          <span>secondary</span>
-        </label>
-        <label className="flex items-center gap-2">
-          <Radio name="actify" value="nuxt" color="tertiary" />
-          <span>tertiary</span>
-        </label>
-        <label className="flex items-center gap-2">
-          <Radio name="actify" value="next" color="error" />
-          <span>error</span>
-        </label>
-        <label className="flex items-center gap-2">
-          <Radio name="actify" value="next" color="error" disabled />
-          <span>disabled</span>
-        </label>
+        <div className="flex items-center gap-2">
+          <Radio defaultValue="React">
+            <Radio.Item value="React">primary</Radio.Item>
+            <Radio.Item value="Vue" color="secondary">
+              secondary
+            </Radio.Item>
+            <Radio.Item value="Actify" color="tertiary">
+              tertiary
+            </Radio.Item>
+            <Radio.Item value="Next.js" color="error">
+              error
+            </Radio.Item>
+            <Radio.Item value="Nuxt" disabled>
+              disabled
+            </Radio.Item>
+          </Radio>
+        </div>
       </div>
       <div className="grid grid-cols-[repeat(auto-fill,minmax(160px,1fr))] gap-2">
         <label className="flex items-center gap-2">
