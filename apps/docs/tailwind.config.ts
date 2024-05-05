6cc9c1d3a690cb81c1e22b9d348ae233878c2f52
lerte smith <lerte@qq.com>
2024-05-05 16:07:53 +0800

    add mdx content to tailwind content

--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -6,6 +6,7 @@ export default withMaterialColors(
     darkMode: 'class',
     content: [
       './pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './content/**/*.{js,ts,jsx,tsx,mdx}',
       './components/**/*.{js,ts,jsx,tsx,mdx}',
       './app/**/*.{js,ts,jsx,tsx,mdx}',
       './node_modules/actify/**/*.tsx'
