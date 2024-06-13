ea52099cda2124c31111f0a369a2d8838509f7fa
lerte smith <lerte@qq.com>
2024-06-13 15:08:49 +0800

    fix build

--- apps/docs/src/components/App.tsx
+++ apps/docs/src/components/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import packageInfo from 'actify/package.json'
 
 export default function App({ children }: { children: React.ReactNode }) {
   return (
     <div
       id="actify"
+      data-version={packageInfo.version}
       className="grid grid-cols-1 antialiased min-h-screen grid-rows-[64px_1fr_56px]"
     >
       {children}
