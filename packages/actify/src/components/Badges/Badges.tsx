621f6cdd6e8d1c5d849d8fb3e09a258fd7bb0085
lerte smith <lerte@qq.com>
2024-07-10 22:27:23 +0800

    badge remove tailwindcss

--- packages/actify/src/components/Badges/Badges.tsx
+++ packages/actify/src/components/Badges/Badges.tsx
@@ -2,38 +2,8 @@
 
 import React, { useMemo } from 'react'
 
-import { tv } from 'tailwind-variants'
-
-const root = tv({
-  base: [
-    'p-1',
-    'grid',
-    'text-xs',
-    'min-w-6',
-    'min-h-6',
-    'top-[4%]',
-    'absolute',
-    'text-white',
-    'right-[2%]',
-    'font-medium',
-    'leading-none',
-    'rounded-full',
-    'translate-x-1/2',
-    '-translate-y-1/2',
-    'place-items-center'
-  ],
-  variants: {
-    color: {
-      primary: 'bg-primary',
-      secondary: 'bg-secondary',
-      tertiary: 'bg-tertiary',
-      error: 'bg-error'
-    }
-  },
-  defaultVariants: {
-    color: 'error'
-  }
-})
+import clsx from 'clsx'
+import styles from './badges.module.css'
 
 interface BadgeProps extends React.ComponentProps<'div'> {
   value?: string | number
@@ -41,7 +11,7 @@ interface BadgeProps extends React.ComponentProps<'div'> {
 }
 
 const Badge = (props: BadgeProps) => {
-  const { color, value = '', className, children, ...rest } = props
+  const { color = 'error', value = '', className, children, ...rest } = props
 
   const badge = useMemo(() => {
     if (parseInt(value.toString()) > 999) {
@@ -51,10 +21,12 @@ const Badge = (props: BadgeProps) => {
     }
   }, [value])
 
+  const classes = clsx(styles['badge'], styles[color], className)
+
   return (
-    <div className="inline-flex relative">
+    <div className={styles['root']}>
       {children}
-      <span className={root({ color, className })} {...rest}>
+      <span className={classes} {...rest}>
         {badge}
       </span>
     </div>
