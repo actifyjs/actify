d1923d888bedad1fdc618f51dc7a86c3ce837cf2
lerte smith <lerte@qq.com>
2024-10-18 08:55:25 +0800

    docs: update sponsor

--- apps/docs/src/components/Sponsors.tsx
+++ apps/docs/src/components/Sponsors.tsx
@@ -1,13 +1,12 @@
 import { Button } from 'actify'
 import NgrokerLogo from '@/components/NgrokerLogo'
-import QzyLogo from '@/components/QzyLogo'
 import TaildoorLogo from '@/components/TaildoorLogo'
 
 const Sponsors = () => {
   return (
     <div className="max-w-7xl mx-auto">
       <h2 className="text-secondary text-2xl mb-4 text-center">Sponsors</h2>
-      <div className="flex gap-2 items-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 items-center">
         <a tabIndex={-1} target="_blank" href="https://www.taildoor.com">
           <Button
             variant="text"
@@ -22,14 +21,6 @@ const Sponsors = () => {
             className="[--md-text-button-container-height:64px] [--md-text-button-container-shape:0]"
           >
             <NgrokerLogo />
-          </Button>
-        </a>
-        <a tabIndex={-1} target="_blank" href="https://www.qzyidc.com">
-          <Button
-            variant="text"
-            className="[--md-text-button-container-height:52px] [--md-text-button-container-shape:0]"
-          >
-            <QzyLogo />
           </Button>
         </a>
       </div>
