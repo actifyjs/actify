5fbcdf4140decc1c64561f1d7283e146bfda47f7
lerte smith <lerte@qq.com>
2024-11-17 22:53:57 +0800

    Divider Component

--- apps/docs/src/usages/divider.tsx
+++ apps/docs/src/usages/divider.tsx
@@ -3,7 +3,16 @@ import { Divider } from 'actify'
 export default () => {
   return (
     <div className="flex flex-col gap-2">
-      <Divider insetStart insetEnd />
+      <div className="flex flex-col">
+        Content above
+        <Divider />
+        Content below
+      </div>
+      <div className="flex">
+        Content left
+        <Divider orientation="vertical" />
+        Content right
+      </div>
     </div>
   )
 }
--- packages/actify/src/components/Divider/Divider.tsx
+++ packages/actify/src/components/Divider/Divider.tsx
@@ -1,23 +1,36 @@
+import { SeparatorProps, useSeparator } from 'react-aria'
+
 import React from 'react'
+import { StyleProps } from '../../utils'
 import clsx from 'clsx'
 import styles from './divider.module.css'
 
-interface DividerProps extends Omit<React.ComponentProps<'div'>, 'children'> {
+interface DividerProps extends SeparatorProps, StyleProps {
   inset?: boolean
   insetStart?: boolean
   insetEnd?: boolean
 }
 const Divider = (props: DividerProps) => {
-  const { className, inset, insetStart, insetEnd, ...rest } = props
+  const {
+    className,
+    inset,
+    insetStart,
+    insetEnd,
+    orientation = 'horizontal'
+  } = props
+
+  const { separatorProps } = useSeparator(props)
 
   return (
     <div
-      {...rest}
+      {...separatorProps}
       className={clsx(
         styles['divider'],
         inset && styles['inset'],
         insetStart && styles['inset-start'],
-        insetEnd && styles['inset-end']
+        insetEnd && styles['inset-end'],
+        styles[orientation],
+        className
       )}
     />
   )
