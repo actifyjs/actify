d868a9d2fc8a80019eedf507f1d74d9fa7e9a91c
lerte smith <lerte@qq.com>
2024-11-21 00:44:28 +0800

    remove asLink

--- apps/docs/src/components/Sponsors.tsx
+++ apps/docs/src/components/Sponsors.tsx
@@ -1,4 +1,4 @@
-import { Button } from 'actify'
+import { LinkButton } from 'actify'
 import NgrokerLogo from '@/components/NgrokerLogo'
 import TaildoorLogo from '@/components/TaildoorLogo'
 
@@ -8,22 +8,20 @@ const Sponsors = () => {
       <h2 className="text-secondary text-2xl mb-4 text-center">Sponsors</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 items-center">
         <a tabIndex={-1} target="_blank" href="https://www.taildoor.com">
-          <Button
-            asLink
+          <LinkButton
             variant="text"
             className="[--md-text-button-container-height:64px] [--md-text-button-container-shape:0]"
           >
             <TaildoorLogo />
-          </Button>
+          </LinkButton>
         </a>
         <a tabIndex={-1} target="_blank" href="https://ngroker.com">
-          <Button
-            asLink
+          <LinkButton
             variant="text"
             className="[--md-text-button-container-height:64px] [--md-text-button-container-shape:0]"
           >
             <NgrokerLogo />
-          </Button>
+          </LinkButton>
         </a>
       </div>
     </div>
